7cac9a067ea258e70f1af9b579505b116dde428e
Besmira Lushi <belushi22@beder.edu.al>
2025-04-23 18:23:21 +0200

    My Portfolio

---
+++ send.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "besmiralushi6@email.com"; 
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
+
+    $body = "You have received a message from your contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Subject: $subject\n\n";
+    $body .= "Message:\n$message\n";
+
+    file_put_contents("messages.txt", $body, FILE_APPEND);
+    echo "Your message was saved locally instead of being sent (mail not configured).";
+}
+else {
+    // Handle non-form access
+    echo "This page should only be accessed via the contact form.";}
+
+
+?>
+
